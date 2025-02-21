f6966f27c96888d85d5adfe557db37173ef94c92
Olamikun <lammyaluko@gmail.com>
2025-02-21 11:18:04 -0700

    - Fixed Merge Conflicts

--- Front-end/smartspec-seng401/app/signup/page.tsx
+++ Front-end/smartspec-seng401/app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "@/components/signUpForm";
 
 const SignupPage = () => {
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+		<div className="flex items-center justify-center min-h-screen bg-primaryColor">
 			<SignUpForm />
 		</div>
 	);
