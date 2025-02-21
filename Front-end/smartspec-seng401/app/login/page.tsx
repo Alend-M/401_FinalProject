d20e7e7ad3d1c458c94f48b8e583c386b1dca791
Olamikun <lammyaluko@gmail.com>
2025-02-21 13:49:13 -0700

    - Created custom Title component - Replaced Login page with styled version

--- Front-end/smartspec-seng401/app/login/page.tsx
+++ Front-end/smartspec-seng401/app/login/page.tsx
@@ -1,11 +1,11 @@
 // import SignInForm from "@/components/signInForm";
-import TestForm from "@/components/testForm";
+import LoginForm from "@/components/LoginForm";
 import React from "react";
 
 const LoginPage = () => {
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-secondaryColor">
-			<TestForm />
+			<LoginForm />
 		</div>
 	);
 };
