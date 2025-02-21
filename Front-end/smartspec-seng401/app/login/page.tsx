4cfafb33734619770f9579d6e82c7f481affa3f7
Olamikun <lammyaluko@gmail.com>
2025-02-21 12:55:01 -0700

    - Styled a 'testForm' which closely matches the Figma design - Added padding constants to constants.ts

--- Front-end/smartspec-seng401/app/login/page.tsx
+++ Front-end/smartspec-seng401/app/login/page.tsx
@@ -1,10 +1,11 @@
-import SignInForm from "@/components/signInForm";
+// import SignInForm from "@/components/signInForm";
+import TestForm from "@/components/testForm";
 import React from "react";
 
 const LoginPage = () => {
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-primaryColor">
-			<SignInForm />
+		<div className="flex items-center justify-center min-h-screen bg-secondaryColor">
+			<TestForm />
 		</div>
 	);
 };
--- Front-end/smartspec-seng401/components/ui/button.tsx
+++ Front-end/smartspec-seng401/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondaryColor text-secondary-foreground shadow-sm hover:bg-secondaryColor/80",
+          "bg-secondaryColor text-offWhite shadow-sm hover:bg-secondaryColor/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
