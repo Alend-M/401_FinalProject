93b2b6d813cfe037e2ac00dfe77c29989493c73a
calebbourb <calebbourbonnais1@gmail.com>
2025-03-15 20:00:16 -0600

    added cors to other services

--- Back-end/API_container/API_service/app.py
+++ Back-end/API_container/API_service/app.py
@@ -1,8 +1,19 @@
 from fastapi import Request, FastAPI
 from API_service.database_service import *
+from fastapi.middleware.cors import CORSMiddleware
 
 # Creates a instance for FastAPI
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/past_builds/{user_id}")
 async def getPastBuilds(user_id: str):
--- Back-end/LLM_container/LLM_service/app.py
+++ Back-end/LLM_container/LLM_service/app.py
@@ -2,9 +2,19 @@
 from fastapi import Request, FastAPI
 from LLM_service.llm_service import *
 import json
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/generate-text")
 async def generate_text(prompt: Request):
